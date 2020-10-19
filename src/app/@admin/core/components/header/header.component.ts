f2083d6537a08f89c8d723d1c34433f8f6585cc6
El Algoritmo de la Noche <44403136+ElAlgoritmoDeLaNoche@users.noreply.github.com>
2020-10-18 23:06:05 -0500

    Conversion de la plantilla simple siderbar startbootstrap en la plantilla del apartado

--- src/app/@admin/core/components/header/header.component.ts
+++ src/app/@admin/core/components/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-admin-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  toggledValue = true;
 
-  ngOnInit(): void {
+  @Output() toggleChange = new EventEmitter<boolean>();
+
+  toggled(){
+    if (this.toggledValue === undefined) {
+      this.toggledValue = true;
+    }
+    this.toggledValue = !this.toggledValue;
+    console.log(this.toggledValue);
+    this.toggleChange.emit(this.toggledValue);
   }
-
 }
